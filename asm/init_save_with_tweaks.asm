b27334d9efc2a98f7fa81c5f0905c0c76c4d3656
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-04-02 23:23:05 -0400

    Start player with wind waker, wind's requiem, and some more event flags

--- asm/init_save_with_tweaks.asm
+++ asm/init_save_with_tweaks.asm
@@ -3,40 +3,46 @@
 .globl _start
 _start:
 
-# Function start stuff
+#; Function start stuff
 stwu sp, -0x10 (sp)
 mflr r0
 stw r0, 0x14 (sp)
 
 
-bl init__10dSv_save_cFv # To call this custom func we overwrote a call to init__10dSv_save_cFv, so call that now.
+bl init__10dSv_save_cFv #; To call this custom func we overwrote a call to init__10dSv_save_cFv, so call that now.
 
 
 bl item_func_sword__Fv
 bl item_func_shield__Fv
 bl item_func_normal_sail__Fv
+bl item_func_wind_tact__Fv #; Wind Waker
+bl item_func_tact_song1__Fv #; Wind's Requiem
 
 
 lis r3, 0x803C
 addi r3, r3, 0x522C
-li r4, 0x0F80 # KORL_UNLOCKED_AND_SPAWN_ON_WINDFALL
+li r4, 0x3510 #; HAS_SEEN_INTRO
 bl onEventBit__11dSv_event_cFUs
-li r4, 0x0908 # SAIL_INTRODUCTION_TEXT_AND_MAP_UNLOCKED
+li r4, 0x2A80 #; HAS_HEROS_CLOTHES
 bl onEventBit__11dSv_event_cFUs
-li r4, 0x2A08 # ENTER_KORL_FOR_THE_FIRST_TIME_AND_SPAWN_ANYWHERE
+li r4, 0x0F80 #; KORL_UNLOCKED_AND_SPAWN_ON_WINDFALL
 bl onEventBit__11dSv_event_cFUs
-li r4, 0x2A80 # HAS_HEROS_CLOTHES
+li r4, 0x0908 #; SAIL_INTRODUCTION_TEXT_AND_MAP_UNLOCKED
 bl onEventBit__11dSv_event_cFUs
-li r4, 0x3510 # HAS_SEEN_INTRO
+li r4, 0x2A08 #; ENTER_KORL_FOR_THE_FIRST_TIME_AND_SPAWN_ANYWHERE
+bl onEventBit__11dSv_event_cFUs
+li r4, 0x0902 #; SAW_DRAGON_ROOST_ISLAND_INTRO
+bl onEventBit__11dSv_event_cFUs
+li r4, 0x0A20 #; WATCHED_FOREST_HAVEN_INTRO_CUTSCENE
 bl onEventBit__11dSv_event_cFUs
 
 lis r3, 0x803C
 addi r3, r3, 0x5D60
-li r4, 0x0310 # Saw event where Grandma gives you the Hero's Clothes
+li r4, 0x0310 #; Saw event where Grandma gives you the Hero's Clothes
 bl onEventBit__11dSv_event_cFUs
 
 
-# Function end stuff
+#; Function end stuff
 lwz r0, 0x14 (sp)
 mtlr r0
 addi sp, sp, 0x10
