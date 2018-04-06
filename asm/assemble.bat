ae46d461513767c213f7788af5f8611211ea1d6e
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-04-06 19:02:50 -0400

    Made a better assembler script

--- asm/assemble.bat
+++ asm/assemble.bat
@@ -1,7 +1,1 @@
-
-C:\devkitPro\devkitPPC\bin\powerpc-eabi-as.exe -mregnames -m750cl init_save_with_tweaks.asm -o init_save_with_tweaks.o
-if %errorlevel% neq 0 exit /b %errorlevel%
-C:\devkitPro\devkitPPC\bin\powerpc-eabi-ld.exe -Ttext 0x803FCFA8 -T linker.ld --oformat binary init_save_with_tweaks.o -o ./init_save_with_tweaks.bin
-if %errorlevel% neq 0 exit /b %errorlevel%
-del init_save_with_tweaks.o
-if %errorlevel% neq 0 exit /b %errorlevel%
+python assemble.py
---
+++ asm/assemble.py
@@ -0,0 +1,81 @@
+
+import glob
+import re
+from subprocess import call
+import os
+import tempfile
+import shutil
+
+temp_dir = tempfile.mkdtemp()
+print(temp_dir)
+print()
+
+try:
+  with open("linker.ld") as f:
+    linker_script = f.read()
+  
+  for filename in glob.glob('*.asm'):
+    print("Assembling " + filename)
+    with open(filename) as f:
+      asm = f.read()
+    
+    temp_asm = ""
+    temp_linker_script = linker_script + "\n"
+    org_offset = None
+    for line in asm.splitlines():
+      line = line.strip()
+      line = line.replace(";", "#")
+      
+      org_match = re.match(r"\.org\s+0x([0-9a-f]+)$", line, re.IGNORECASE)
+      branch_match = re.match(r"(?:b|beq|bne|blt|bgt|ble|bge)\s+0x([0-9a-f]+)(?:$|\s)", line, re.IGNORECASE)
+      if org_match and not org_offset:
+        org_offset = int(org_match.group(1), 16)
+      elif org_match and org_offset:
+        raise Exception("Found more than one .org directive")
+      elif branch_match:
+        # Replace branches to specific addresses with labels, and define the address of those labels in the linker script.
+        branch_dest = int(branch_match.group(1), 16)
+        branch_temp_label = "branch_label_%X" % branch_dest
+        temp_linker_script += "%s = 0x%X;" % (branch_temp_label, branch_dest)
+        line = re.sub(r"0x" + branch_match.group(1), branch_temp_label, line, 1)
+        temp_asm += line + "\n"
+      else:
+        temp_asm += line + "\n"
+    
+    if org_offset is None:
+      raise Exception("Could not find .org directive")
+    
+    temp_asm_name = os.path.join(temp_dir, "tmp_" + filename)
+    with open(temp_asm_name, "w") as f:
+      f.write(temp_asm)
+    temp_linker_name = os.path.join(temp_dir, "tmp_linker.ld")
+    with open(temp_linker_name, "w") as f:
+      f.write(temp_linker_script)
+    
+    o_name = os.path.join(temp_dir, "tmp_" + os.path.splitext(filename)[0] + ".o")
+    command = [
+      r"C:\devkitPro\devkitPPC\bin\powerpc-eabi-as.exe", "-mregnames", "-m750cl",
+      temp_asm_name, "-o", o_name
+    ]
+    print(" ".join(command))
+    print()
+    result = call(command)
+    if result != 0:
+      raise Exception("Assembler call failed")
+    
+    bin_name = os.path.splitext(filename)[0] + ".bin"
+    command = [
+      r"C:\devkitPro\devkitPPC\bin\powerpc-eabi-ld.exe",
+      "-Ttext", "%X" % org_offset,
+      "-T", temp_linker_name,
+      "--oformat", "binary",
+      o_name,
+      "-o", bin_name
+    ]
+    print(" ".join(command))
+    print()
+    result = call(command)
+    if result != 0:
+      raise Exception("Linker call failed")
+finally:
+  shutil.rmtree(temp_dir)
