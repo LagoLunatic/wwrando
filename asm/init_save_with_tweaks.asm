ee44e451e5e5e03433f7fb01ff77b2002886fff9
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-04-12 19:12:21 -0400

    Skip some more forest haven and windfall scenes

--- asm/init_save_with_tweaks.asm
+++ asm/init_save_with_tweaks.asm
@@ -38,6 +38,8 @@ li r4, 0x1F40 ; SAW_QUILL_CUTSCENE_ON_DRI
 bl onEventBit__11dSv_event_cFUs
 li r4, 0x0A20 ; WATCHED_FOREST_HAVEN_INTRO_CUTSCENE
 bl onEventBit__11dSv_event_cFUs
+li r4, 0x1801 ; WATCHED_DEKU_TREE_CUTSCENE
+bl onEventBit__11dSv_event_cFUs
 
 lis r3, 0x803C
 addi r3, r3, 0x5D60
@@ -45,19 +47,24 @@ li r4, 0x0310 ; Saw event where Grandma gives you the Hero's Clothes
 bl onEventBit__11dSv_event_cFUs
 
 
-; Set four switch bits (8B) for several events that happen in the Fairy Woods on Outset.
+; Set four switch bits (0, 1, 3, 7) for several events that happen in the Fairy Woods on Outset.
 ; Setting these switches causes the Tetra hanging from a tree and rescuing her from Bokoblins events to be marked as finished.
-; Also set the switch (200) for having seen the event where you enter the Rito Aerie for the first time and get the Delivery Bag.
+; Also set the switch (9) for having seen the event where you enter the Rito Aerie for the first time and get the Delivery Bag.
+; Also set the the switch (1E) for having seen the intro to the interior of the Forest Haven, where the camera pans up.
 lis r3, 0x803C
 addi r3, r3, 0x5118
-li r4, 0x28B
+lis r4, 0x4000
+addi r4, r4, 0x028B
 stw r4, 0 (r3)
 
-; Set a switch for the event on Greatfish Isle so that the endless night never starts.
+; Set a switch bit (19) for the event on Greatfish Isle so that the endless night never starts.
 lis r3, 0x803C
 addi r3, r3, 0x4F8C
 lis r4, 0x0200
 stw r4, 0 (r3)
+; Also set a switch bit (3F) for having seen the Windfall Island intro scene.
+lis r4, 0x8000
+stw r4, 4 (r3)
 
 
 ; Function end stuff
