042e8628ad7a1bcfb12e347bac0a749be01a9668
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-05-26 16:13:31 -0400

    Don't allow forsaken fortress to be starting island

--- randomizer.py
+++ randomizer.py
@@ -517,7 +517,13 @@ class Randomizer:
           scob.save_changes()
   
   def randomize_starting_island(self):
-    starting_island_room_index = self.rng.randrange(1, 49+1)
+    possible_starting_islands = list(range(1, 49+1))
+    
+    # Don't allow Forsaken Fortress to be the starting island.
+    # It wouldn't really cause problems, but it would be weird because you normally need bombs to get in, and you would need to use Ballad of Gales to get out.
+    possible_starting_islands.remove(1)
+    
+    starting_island_room_index = self.rng.choice(possible_starting_islands)
     tweaks.set_new_game_starting_room_index(self, starting_island_room_index)
     tweaks.change_ship_starting_island(self, starting_island_room_index)
   
