63e59134a0b1d267980d84cd40482f88789cfd60
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-06-10 12:23:49 -0400

    Changed swift sail textures

--- tweaks.py
+++ tweaks.py
@@ -347,6 +347,7 @@ def make_items_progressive(self):
 def make_sail_behave_like_swift_sail(self):
   # Causes the wind direction to always change to face the direction KoRL is facing as long as the sail is out.
   # Also doubles KoRL's speed.
+  # And changes the textures to match the swift sail from HD.
   
   ship_data = self.get_raw_file("files/rels/d_a_ship.rel")
   # Change the relocation for line B9FC, which originally called setShipSailState.
@@ -354,6 +355,39 @@ def make_sail_behave_like_swift_sail(self):
   
   write_float(ship_data, 0xDBE8, 55.0*2) # Sailing speed
   write_float(ship_data, 0xDBC0, 80.0*2) # Initial speed
+  
+  try:
+    from sys import _MEIPASS
+    assets_path = os.path.join(_MEIPASS, "assets")
+  except ImportError:
+    assets_path = "assets"
+  
+  new_sail_tex_image_path = os.path.join(assets_path, "swift sail texture.png")
+  new_sail_icon_image_path = os.path.join(assets_path, "swift sail icon.png")
+  new_sail_itemget_tex_image_path = os.path.join(assets_path, "swift sail item get texture.png")
+  
+  # Modify the sail's texture while sailing.
+  ship_arc = self.get_arc("files/res/Object/Ship.arc")
+  sail_image = ship_arc.get_file("new_ho1.bti")
+  sail_image.replace_image(new_sail_tex_image_path)
+  sail_image.save_changes()
+  
+  # Modify the sail's item icon.
+  itemicon_arc = self.get_arc("files/res/Msg/itemicon.arc")
+  sail_icon_image = itemicon_arc.get_file("sail_00.bti")
+  sail_icon_image.replace_image(new_sail_icon_image_path)
+  sail_icon_image.save_changes()
+  
+  # Modify the sail's item get texture.
+  sail_itemget_arc = self.get_arc("files/res/Object/Vho.arc")
+  sail_itemget_model = sail_itemget_arc.bdl_files[0] # TODO: use filename as index instead of number
+  sail_itemget_tex_image = sail_itemget_model.tex1.textures_by_name["Vho"]
+  # Originally it used image format 0xE, which is lossy DXT1 compression.
+  # But implementing this while having the texture actually look good is too difficult, so instead switch this to image format 9 and palette format 1 (C8 with a 255 color RGB565 palette).
+  sail_itemget_tex_image.image_format = 9
+  sail_itemget_tex_image.palette_format = 1
+  sail_itemget_tex_image.replace_image(new_sail_itemget_tex_image_path)
+  sail_itemget_model.save_changes()
 
 def add_ganons_tower_warp_to_ff2(self):
   # Normally the warp object from Forsaken Fortress down to Ganon's Tower only appears in FF3.
