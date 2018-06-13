e252865a081157638eb1bcc0be359a32e63a4cd6
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-06-13 11:22:45 -0400

    Get taskbar icon to display when running from source too

--- wwrando.py
+++ wwrando.py
@@ -3,8 +3,13 @@ from PySide.QtGui import *
 from PySide.QtCore import *
 
 import sys
+import ctypes
 
 from ui.randomizer_window import WWRandomizerWindow
+
+# Setting the app user model ID is necessary for Windows to display a custom taskbar icon when running the randomizer from source.
+app_id = "LagoLunatic.WindWakerRandomizer"
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
 
 qApp = QApplication(sys.argv)
 window = WWRandomizerWindow()
