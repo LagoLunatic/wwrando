bf27a29d135fc2c59183c8dec86bf21bc729e9b2
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-06-20 18:04:12 -0400

    Remove song replays

--- asm/necessary_fixes.asm
+++ asm/necessary_fixes.asm
@@ -708,3 +708,18 @@
   ; For the 3 Rock Spire Shop Ship slots.
   .int custom_getSelectItemNo_progressive
 .close
+
+
+
+
+; Remove song replays, where Link plays a fancy animation to conduct the song after the player plays it.
+.open "sys/main.dol"
+.org 0x8014ECE0
+  ; Originally checked if the "You conducted..." text box has disappeared.
+  ; Remove that check.
+  nop
+.org 0x8014EF28
+  ; Originally checked if Link's conducting animation has finished playing.
+  ; Remove that check.
+  nop
+.close
