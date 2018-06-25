738276e414c416a5f3d0a580e5b3f6a10b6c2e46
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-06-25 09:22:25 -0400

    Update option descrptions

--- ui/options.py
+++ ui/options.py
@@ -2,32 +2,32 @@
 from collections import OrderedDict
 
 OPTIONS = OrderedDict([
-  ("progression_dungeons", "This controls whether dungeons can contain progress items.\nIf this is not checked, dungeons will only contain optional items you don't need to beat the game."),
-  ("progression_secret_caves", "This controls whether secret caves can contain progress items.\nIf this is not checked, secret caves will only contain optional items you don't need to beat the game."),
-  ("progression_sidequests", "This controls whether sidequests can reward progress items.\nIf this is not checked, sidequests will only reward optional items you don't need to beat the game."),
-  ("progression_minigames", "This controls whether minigames can reward progress items.\nIf this is not checked, minigames will only reward optional items you don't need to beat the game."),
-  ("progression_platforms_rafts", "This controls whether lookout platforms and rafts can contain progress items.\nIf this is not checked, lookout platforms and rafts will only contain optional items you don't need to beat the game."),
-  ("progression_submarines", "This controls whether submarines can contain progress items.\nIf this is not checked, submarines will only contain optional items you don't need to beat the game."),
-  ("progression_eye_reef_chests", "This controls whether the chests that appear after clearing out the eye reefs can contain progress items.\nIf this is not checked, they will only contain optional items you don't need to beat the game."),
-  ("progression_big_octos_gunboats", "This controls whether the items dropped by Big Octos and Gunboats can contain progress items.\nIf this is not checked, they will only contain optional items you don't need to beat the game."),
-  ("progression_triforce_charts", "This controls whether the sunken treasure chests marked on Triforce Charts can contain progress items.\nIf this is not checked, they will only contain optional items you don't need to beat the game."),
-  ("progression_treasure_charts", "This controls whether the sunken treasure chests marked on Treasure Charts can contain progress items.\nIf this is not checked, they will only contain optional items you don't need to beat the game."),
-  ("progression_expensive_purchases", "This controls whether items that must be bought for a large number of rupees (100+) can be progress items.\nIf this is not checked, they will only be optional items you don't need to beat the game."),
-  ("progression_gifts", "This controls whether gifts freely given by NPCs can be progress items.\nIf this is not checked, they will only be optional items you don't need to beat the game."),
-  ("progression_mail", "This controls whether mail can contain progress items.\nIf this is not checked, mail will only contain optional items you don't need to beat the game."),
-  ("progression_misc", "Miscellaneous locations that don't fit into any of the above categories.\nIf this is not checked, they will only contain optional items you don't need to beat the game."),
+  ("progression_dungeons", "This controls whether dungeons can contain progress items.<br><u>If this is not checked, dungeons will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_secret_caves", "This controls whether secret caves can contain progress items.<br><u>If this is not checked, secret caves will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_sidequests", "This controls whether sidequests can reward progress items.<br><u>If this is not checked, sidequests will still be randomized</u>, but will only reward optional items you don't need to beat the game."),
+  ("progression_minigames", "This controls whether minigames can reward progress items.<br><u>If this is not checked, minigames will still be randomized</u>, but will only reward optional items you don't need to beat the game."),
+  ("progression_platforms_rafts", "This controls whether lookout platforms and rafts can contain progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_submarines", "This controls whether submarines can contain progress items.<br><u>If this is not checked, submarines will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_eye_reef_chests", "This controls whether the chests that appear after clearing out the eye reefs can contain progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_big_octos_gunboats", "This controls whether the items dropped by Big Octos and Gunboats can contain progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_triforce_charts", "This controls whether the sunken treasure chests marked on Triforce Charts can contain progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_treasure_charts", "This controls whether the sunken treasure chests marked on Treasure Charts can contain progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_expensive_purchases", "This controls whether items that must be bought for a large number of rupees (100+) can be progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only be optional items you don't need to beat the game."),
+  ("progression_gifts", "This controls whether gifts freely given by NPCs can be progress items.<br><u>If this is not checked, they will still be randomized</u>, but will only be optional items you don't need to beat the game."),
+  ("progression_mail", "This controls whether mail can contain progress items.<br><u>If this is not checked, mail will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
+  ("progression_misc", "Miscellaneous locations that don't fit into any of the above categories.<br><u>If this is not checked, they will still be randomized</u>, but will only contain optional items you don't need to beat the game."),
   
   ("keylunacy", "Allows dungeon keys (as well as maps and compasses) to appear anywhere in the game, not just in the dungeon they're for."),
   
   ("randomize_dungeon_entrances", "Shuffles around which dungeon entrances take you into which dungeons. (No effect on Forsaken Fortress or Ganon's Tower.)"),
-  ("randomize_charts", "Randomizes which sectors are drawn on each Triforce/Treasure Chart."),
+  ("randomize_charts", "Randomizes which sector is drawn on each Triforce/Treasure Chart."),
   ("randomize_starting_island", "Randomizes which island you start the game on."),
   
   ("swift_sail", "Sailing speed is doubled and the direction of the wind is always at your back as long as the sail is out."),
   
-  ("instant_text_boxes", "Text appears instantly.\nAlso, the B button is changed to instantly skip through text as long as you hold it down."),
+  ("instant_text_boxes", "Text appears instantly.<br>Also, the B button is changed to instantly skip through text as long as you hold it down."),
   
   ("reveal_full_sea_chart", "Start the game with the sea chart fully drawn out."),
   
-  ("num_starting_triforce_shards", "Change the number of Triforce Shards you start the game with.\nThe higher you set this, the fewer you will need to find placed randomly."),
+  ("num_starting_triforce_shards", "Change the number of Triforce Shards you start the game with.<br>The higher you set this, the fewer you will need to find placed randomly."),
 ])
