ce0dafd9de0ec73b0fb86077dbe9add8e93efa45
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-06-28 14:18:33 -0400

    Refactor dungeon entrance rando to use named tuples

--- randomizers/dungeon_entrances.py
+++ randomizers/dungeon_entrances.py
@@ -1,19 +1,28 @@
 
+from collections import namedtuple
+
+DungeonEntrance = namedtuple(
+  "DungeonEntrance",
+  "stage_name room_num scls_exit_index spawn_id entrance_name island_name warp_out_stage_name warp_out_room_num warp_out_spawn_id"
+)
 DUNGEON_ENTRANCES = [
-  # Stage name, room index, SCLS entry index, spawn ID when exiting, entrance name for macro, island name entrance is on, post-boss warp out destination stage name, room number, and spawn ID
-  ("Adanmae", 0, 2, 2, "Dungeon Entrance On Dragon Roost Island", "Dragon Roost Island", "sea", 13, 211),
-  ("sea", 41, 6, 6, "Dungeon Entrance In Forest Haven Sector", "Forest Haven", "Omori", 0, 215),
-  ("sea", 26, 0, 2, "Dungeon Entrance In Tower of the Gods Sector", "Tower of the Gods", "sea", 26, 1),
-  ("Edaichi", 0, 0, 1, "Dungeon Entrance On Headstone Island", "Headstone Island", "sea", 45, 229),
-  ("Ekaze", 0, 0, 1, "Dungeon Entrance On Gale Isle", "Gale Isle", "sea", 4, 232),
+  DungeonEntrance("Adanmae", 0, 2, 2, "Dungeon Entrance On Dragon Roost Island", "Dragon Roost Island", "sea", 13, 211),
+  DungeonEntrance("sea", 41, 6, 6, "Dungeon Entrance In Forest Haven Sector", "Forest Haven", "Omori", 0, 215),
+  DungeonEntrance("sea", 26, 0, 2, "Dungeon Entrance In Tower of the Gods Sector", "Tower of the Gods", "sea", 26, 1),
+  DungeonEntrance("Edaichi", 0, 0, 1, "Dungeon Entrance On Headstone Island", "Headstone Island", "sea", 45, 229),
+  DungeonEntrance("Ekaze", 0, 0, 1, "Dungeon Entrance On Gale Isle", "Gale Isle", "sea", 4, 232),
 ]
+
+DungeonExit = namedtuple(
+  "DungeonExit",
+  "stage_name room_num scls_exit_index spawn_id dungeon_name boss_stage_name"
+)
 DUNGEON_EXITS = [
-  # Stage name, room index, SCLS entry index, spawn ID when entering, dungeon name for macro, boss stage name
-  ("M_NewD2", 0, 0, 0, "Dragon Roost Cavern", "M_DragB"),
-  ("kindan", 0, 0, 0, "Forbidden Woods", "kinBOSS"),
-  ("Siren", 0, 1, 0, "Tower of the Gods", "SirenB"),
-  ("M_Dai", 0, 0, 0, "Earth Temple", "M_DaiB"),
-  ("kaze", 15, 0, 15, "Wind Temple", "kazeB"),
+  DungeonExit("M_NewD2", 0, 0, 0, "Dragon Roost Cavern", "M_DragB"),
+  DungeonExit("kindan", 0, 0, 0, "Forbidden Woods", "kinBOSS"),
+  DungeonExit("Siren", 0, 1, 0, "Tower of the Gods", "SirenB"),
+  DungeonExit("M_Dai", 0, 0, 0, "Earth Temple", "M_DaiB"),
+  DungeonExit("kaze", 15, 0, 15, "Wind Temple", "kazeB"),
 ]
 
 def randomize_dungeon_entrances(self):
@@ -31,52 +40,50 @@ def randomize_dungeon_entrances(self):
     should_limit_first_dungeon_possibilities = False
   
   remaining_exits = DUNGEON_EXITS.copy()
-  for entrance_stage_name, entrance_room_index, entrance_scls_index, entrance_spawn_id, entrance_name, island_name, warp_out_stage_name, warp_out_room_number, warp_out_spawn_id in DUNGEON_ENTRANCES:
-    if should_limit_first_dungeon_possibilities and entrance_name == "Dungeon Entrance On Dragon Roost Island":
+  for dungeon_entrance in DUNGEON_ENTRANCES:
+    if should_limit_first_dungeon_possibilities and dungeon_entrance.entrance_name == "Dungeon Entrance On Dragon Roost Island":
       possible_remaining_exits = []
-      for exit_tuple in remaining_exits:
-        _, _, _, _, dungeon_name, _ = exit_tuple
-        if dungeon_name in ["Dragon Roost Cavern"]:
-          possible_remaining_exits.append(exit_tuple)
+      for dungeon_exit in remaining_exits:
+        if dungeon_exit.dungeon_name in ["Dragon Roost Cavern"]:
+          possible_remaining_exits.append(dungeon_exit)
     else:
       possible_remaining_exits = remaining_exits
     
-    random_dungeon_exit = self.rng.choice(possible_remaining_exits)
-    remaining_exits.remove(random_dungeon_exit)
-    exit_stage_name, exit_room_index, exit_scls_index, exit_spawn_id, dungeon_name, boss_stage_name = random_dungeon_exit
+    dungeon_exit = self.rng.choice(possible_remaining_exits)
+    remaining_exits.remove(dungeon_exit)
     
     # Update the dungeon this entrance takes you into.
-    entrance_dzx_path = "files/res/Stage/%s/Room%d.arc" % (entrance_stage_name, entrance_room_index)
+    entrance_dzx_path = "files/res/Stage/%s/Room%d.arc" % (dungeon_entrance.stage_name, dungeon_entrance.room_num)
     entrance_dzx = self.get_arc(entrance_dzx_path).dzx_files[0]
-    entrance_scls = entrance_dzx.entries_by_type("SCLS")[entrance_scls_index]
-    entrance_scls.dest_stage_name = exit_stage_name
-    entrance_scls.room_index = exit_room_index
-    entrance_scls.spawn_id = exit_spawn_id
+    entrance_scls = entrance_dzx.entries_by_type("SCLS")[dungeon_entrance.scls_exit_index]
+    entrance_scls.dest_stage_name = dungeon_exit.stage_name
+    entrance_scls.room_index = dungeon_exit.room_num
+    entrance_scls.spawn_id = dungeon_exit.spawn_id
     entrance_scls.save_changes()
     
     # Update the entrance you're put at when leaving the dungeon.
-    exit_dzx_path = "files/res/Stage/%s/Room%d.arc" % (exit_stage_name, exit_room_index)
+    exit_dzx_path = "files/res/Stage/%s/Room%d.arc" % (dungeon_exit.stage_name, dungeon_exit.room_num)
     exit_dzx = self.get_arc(exit_dzx_path).dzx_files[0]
-    exit_scls = exit_dzx.entries_by_type("SCLS")[exit_scls_index]
-    exit_scls.dest_stage_name = entrance_stage_name
-    exit_scls.room_index = entrance_room_index
-    exit_scls.spawn_id = entrance_spawn_id
+    exit_scls = exit_dzx.entries_by_type("SCLS")[dungeon_exit.scls_exit_index]
+    exit_scls.dest_stage_name = dungeon_entrance.stage_name
+    exit_scls.room_index = dungeon_entrance.room_num
+    exit_scls.spawn_id = dungeon_entrance.spawn_id
     exit_scls.save_changes()
     
     # Update the wind warp out event to take you to the correct island.
-    boss_stage_arc_path = "files/res/Stage/%s/Stage.arc" % boss_stage_name
+    boss_stage_arc_path = "files/res/Stage/%s/Stage.arc" % dungeon_exit.boss_stage_name
     event_list = self.get_arc(boss_stage_arc_path).event_list_files[0]
     warp_out_event = event_list.events_by_name["WARP_WIND_AFTER"]
     director = next(actor for actor in warp_out_event.actors if actor.name == "DIRECTOR")
     stage_change_action = next(action for action in director.actions if action.name == "NEXT")
     stage_name_prop = next(prop for prop in stage_change_action.properties if prop.name == "Stage")
-    event_list.set_property_value(stage_name_prop.property_index, warp_out_stage_name)
+    event_list.set_property_value(stage_name_prop.property_index, dungeon_entrance.warp_out_stage_name)
     room_num_prop = next(prop for prop in stage_change_action.properties if prop.name == "RoomNo")
-    event_list.set_property_value(room_num_prop.property_index, warp_out_room_number)
+    event_list.set_property_value(room_num_prop.property_index, dungeon_entrance.warp_out_room_num)
     spawn_id_prop = next(prop for prop in stage_change_action.properties if prop.name == "StartCode")
-    event_list.set_property_value(spawn_id_prop.property_index, warp_out_spawn_id)
+    event_list.set_property_value(spawn_id_prop.property_index, dungeon_entrance.warp_out_spawn_id)
     
-    self.dungeon_entrances[entrance_name] = dungeon_name
-    self.dungeon_island_locations[dungeon_name] = island_name
+    self.dungeon_entrances[dungeon_entrance.entrance_name] = dungeon_exit.dungeon_name
+    self.dungeon_island_locations[dungeon_exit.dungeon_name] = dungeon_entrance.island_name
   
   self.logic.update_dungeon_entrance_macros()
