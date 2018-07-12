c3d552bcba1ae8f3bec74750245e2634273a2a90
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-07-12 12:44:47 -0400

    Don't include version in executable name

--- build.py
+++ build.py
@@ -3,9 +3,10 @@ from zipfile import ZipFile
 
 from randomizer import VERSION
 
-base_name = "Wind Waker Randomizer " + VERSION
-zip_name = base_name.replace(" ", "_") + ".zip"
+base_name = "Wind Waker Randomizer"
+base_name_with_version = base_name + " " + VERSION
+zip_name = base_name_with_version.replace(" ", "_") + ".zip"
 
 with ZipFile("./dist/" + zip_name, "w") as zip:
-  zip.write("./dist/%s.exe" % base_name, arcname="%s.exe" % base_name)
+  zip.write("./dist/%s.exe" % base_name_with_version, arcname="%s.exe" % base_name)
   zip.write("README.txt")
