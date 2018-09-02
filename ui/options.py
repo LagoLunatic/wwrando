662e9e829d22288fc820046fb083c70c154fa50b
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-09-01 20:53:19 -0400

    Change sword mode description

--- ui/options.py
+++ ui/options.py
@@ -119,7 +119,7 @@ OPTIONS = OrderedDict([
   ),
   (
     "sword_mode",
-    "Controls whether you start with the Hero's Sword, the Hero's Sword is randomized, or if there are no swords in the entire game."
+    "Controls whether you start with the Hero's Sword, the Hero's Sword is randomized, or if there are no swords in the entire game.\nSwordless and Randomized Sword are challenge modes, not recommended for your first run."
   ),
   (
     "skip_rematch_bosses",
