3e9d1b924198edbe61084e0530cd5eeb934c9d9f
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-10-06 15:09:58 -0400

    Randomize the reward for finding all 5 tingle statues

--- logic/item_types.py
+++ logic/item_types.py
@@ -43,6 +43,12 @@ PROGRESS_ITEMS = [
   "Moblin's Letter",
   "Cabana Deed",
   
+  "Dragon Tingle Statue",
+  "Forbidden Tingle Statue",
+  "Goddess Tingle Statue",
+  "Earth Tingle Statue",
+  "Wind Tingle Statue",
+  
   "Magic Meter Upgrade",
   
   "Ghost Ship Chart",
@@ -62,13 +68,6 @@ NONPROGRESS_ITEMS = [
   # If you got a progress item for your delivery bag when the bag was already full, the new item would simply never enter your inventory.
   #"Complimentary ID",
   "Fill-Up Coupon",
-  
-  # Tingle statues don't randomize properly, they don't count as owned if you find them in a random place.
-  #"Dragon Tingle Statue",
-  #"Forbidden Tingle Statue",
-  #"Goddess Tingle Statue",
-  #"Earth Tingle Statue",
-  #"Wind Tingle Statue",
   
   "Hurricane Spin",
   
