948d692147b287e38b93265694101e731eec38c3
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-10-10 22:39:30 -0400

    Mark 32 bit builds as not recommended

--- build.py
+++ build.py
@@ -9,10 +9,12 @@ base_name_with_version = base_name + " " + VERSION_WITHOUT_COMMIT
 import struct
 if (struct.calcsize("P") * 8) == 64:
   base_name_with_version += "_64bit"
+  base_zip_name = base_name_with_version
 else:
   base_name_with_version += "_32bit"
+  base_zip_name = base_name_with_version + "_(not_recommended)"
 
-zip_name = base_name_with_version.replace(" ", "_") + ".zip"
+zip_name = base_zip_name.replace(" ", "_") + ".zip"
 
 with ZipFile("./dist/" + zip_name, "w") as zip:
   zip.write("./dist/%s.exe" % base_name_with_version, arcname="%s.exe" % base_name)
