0285f2e6b37b2e4351c321e0def719453e2f2114
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-02-28 15:26:32 -0500

    Prevent DRC entrance lava from solidifying

--- asm/necessary_fixes.asm
+++ asm/necessary_fixes.asm
@@ -1152,3 +1152,15 @@
 .org 0x800C3420 ; In item_func_camera2__Fv (Deluxe Picto Box item get func)
   b deluxe_picto_box_item_func_fix_equipped_picto_box
 .close
+
+
+
+
+; Prevent the lava outside the entrance to DRC from solidifying once you own Din's Pearl.
+; This is so the puzzle with throwing the bomb flowers doesn't become pointless.
+.open "files/rels/d_a_obj_eayogn.rel" ; Solidified lava object
+.org 0x40C ; Start of check_ev_bit__13daObjEayogn_cCFv
+  ; Always return false
+  li r3, 0
+  blr
+.close
