1fc000bd393b343abc83dc39150f78c0f3dd7d6b
nbouteme <nbouteme@users.noreply.github.com>
2019-03-18 14:53:27 +0100

    Separate inventory categories

--- wwr_ui/inventory.py
+++ wwr_ui/inventory.py
@@ -1,7 +1,7 @@
 # Can't use logic's PROGRESS_ITEMS because we don't want all items
 # to be accessible early and remove too much fun
 # And also because progressive items require special handling
-INVENTORY_ITEMS = [
+REGULAR_ITEMS = [
   "Telescope",
   "Magic Armor",
   "Hero's Charm",
@@ -15,8 +15,15 @@ INVENTORY_ITEMS = [
   "Deku Leaf",
   "Mirror Shield",
   "Hurricane Spin",
-] + ["Progressive Bow"]      * 3 + \
-    ["Progressive Wallet"]   * 2
+]
 
+PROGRESSIVE_ITEMS = \
+    ["Progressive Bow"]       * 3 + \
+    ["Progressive Quiver"]    * 2 + \
+    ["Progressive Bomb Bag"]  * 2 + \
+    ["Progressive Wallet"]    * 2 + \
+    ["Progressive Picto Box"] * 2
+
+INVENTORY_ITEMS = REGULAR_ITEMS + PROGRESSIVE_ITEMS
 # Not sure how the Progressive Bomb Bag and Quiver play with their
-# respective items themselves so to be safe they aren't supported yet. 
+# respective items themselves so to be safe they aren't supported yet.
