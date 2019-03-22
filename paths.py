d33089326a979d4feceb1098a6c8c2fcdceb8518
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-03-22 14:59:29 -0400

    Simplify path detection code

--- paths.py
+++ paths.py
@@ -4,15 +4,11 @@ import os
 try:
   from sys import _MEIPASS
   RANDO_ROOT_PATH = _MEIPASS
-  ASSETS_PATH = os.path.join(_MEIPASS, "assets")
-  DATA_PATH = os.path.join(_MEIPASS, "data")
-  LOGIC_PATH = os.path.join(_MEIPASS, "logic")
-  ASM_PATH = os.path.join(_MEIPASS, "asm")
-  SEEDGEN_PATH = os.path.join(_MEIPASS, "seedgen")
 except ImportError:
   RANDO_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
-  ASSETS_PATH = os.path.join(RANDO_ROOT_PATH, "assets")
-  DATA_PATH = os.path.join(RANDO_ROOT_PATH, "data")
-  LOGIC_PATH = os.path.join(RANDO_ROOT_PATH, "logic")
-  ASM_PATH = os.path.join(RANDO_ROOT_PATH, "asm")
-  SEEDGEN_PATH = os.path.join(RANDO_ROOT_PATH, "seedgen")
+
+ASSETS_PATH = os.path.join(RANDO_ROOT_PATH, "assets")
+DATA_PATH = os.path.join(RANDO_ROOT_PATH, "data")
+LOGIC_PATH = os.path.join(RANDO_ROOT_PATH, "logic")
+ASM_PATH = os.path.join(RANDO_ROOT_PATH, "asm")
+SEEDGEN_PATH = os.path.join(RANDO_ROOT_PATH, "seedgen")
