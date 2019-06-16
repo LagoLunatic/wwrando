f3e7041ac1d8508d5d0bbb5c186e3b56a1b62b36
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-06-16 13:26:15 -0400

    Don't check for updates when running the rando from source

--- paths.py
+++ paths.py
@@ -4,8 +4,10 @@ import os
 try:
   from sys import _MEIPASS
   RANDO_ROOT_PATH = _MEIPASS
+  IS_RUNNING_FROM_SOURCE = False
 except ImportError:
   RANDO_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+  IS_RUNNING_FROM_SOURCE = True
 
 ASSETS_PATH = os.path.join(RANDO_ROOT_PATH, "assets")
 DATA_PATH = os.path.join(RANDO_ROOT_PATH, "data")
