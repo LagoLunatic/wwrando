58cdee5111366a8dae8918d04a2ae02fff7d1349
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-06-23 23:19:35 -0400

    Fix format of python 64 command in build script
    For some reason explicitly specifying 64 bit python results in an error on my new PC, even though specifying 32 bit works as normal. Dunno why.

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ set /p key= < keys/build_key.txt
 py -3.6-32 -m PyInstaller wwrando.spec --key=%key%
 py -3.6-32 build.py
 
-py -3.6-64 -m PyInstaller wwrando.spec --key=%key%
-py -3.6-64 build.py
+py -3.6 -m PyInstaller wwrando.spec --key=%key%
+py -3.6 build.py
