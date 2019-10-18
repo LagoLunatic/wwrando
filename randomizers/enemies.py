a89e416e017fd096849bfadbc2904466e78cbd45
LagoLunatic <LagoLunatic@users.noreply.github.com>
2019-10-18 10:48:16 -0400

    Properly set keese to ceiling type when in ceiling location

--- randomizers/enemies.py
+++ randomizers/enemies.py
@@ -659,6 +659,10 @@ def randomize_enemy_params(self, enemy_data, enemy, category, dzx, layer):
     pass
   elif enemy.name in ["keeth", "Fkeeth"]:
     enemy.keese_is_fire_keese = self.rng.choice([0, 1])
+    if category == "Ceiling":
+      enemy.keese_behavior_type = 0
+    else:
+      enemy.keese_behavior_type = 1
   elif enemy.name == "Oq":
     # Freshwater Octorok.
     enemy.octorok_projectile_type = self.rng.choice([
