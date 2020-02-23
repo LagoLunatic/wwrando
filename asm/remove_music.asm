250cfc37775e54df48c436db39bc12b6d419da40
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-02-23 11:51:11 -0500

    Update comments

--- asm/remove_music.asm
+++ asm/remove_music.asm
@@ -8,9 +8,10 @@
   ; Remove combat music
   blr
 .org 0x802A47B8 ; subBgmStart__11JAIZelBasicFUl
-  ; Remove mini-boss music
+  ; Remove mini-boss music and item get jingles
   blr
 .org 0x802A33D0 ; bgmStreamPlay__11JAIZelBasicFv
   ; Remove boss defeat fanfare
   blr
+; (Puzzle solved jingles are considered to be sound effects, and are not removed.)
 .close
