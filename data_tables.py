5b8c0dcb8032caeccd873dd803c2f69c49267d08
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-05 12:50:54 -0500

    Add DataTables class for loading from text lists

---
+++ data_tables.py
@@ -0,0 +1,31 @@
+
+import os
+import yaml
+
+from paths import DATA_PATH
+
+class DataTables:
+  with open(os.path.join(DATA_PATH, "actor_info.txt"), "r") as f:
+    actor_info = yaml.safe_load(f)
+  
+  actor_name_to_class_name = {}
+  for actor_name, actor_info in actor_info.items():
+    class_name = actor_info["Class Name"]
+    if class_name is not None:
+      # Class name is case insensitive.
+      class_name = class_name.lower()
+    actor_name_to_class_name[actor_name] = class_name
+  
+  with open(os.path.join(DATA_PATH, "actor_parameters.txt"), "r") as f:
+    case_sensitive_actor_parameters = yaml.safe_load(f)
+  
+  actor_parameters = {}
+  for class_name, params in case_sensitive_actor_parameters.items():
+    # Class name is case insensitive.
+    class_name = class_name.lower()
+    
+    actor_parameters[class_name] = {}
+    for param_name, param_data in params.items():
+      actor_parameters[class_name][param_name] = (param_data["Bitfield name"], param_data["Mask"])
+  
+  del case_sensitive_actor_parameters
