e045de16a7fe9c25f167b318fa5c4c092e877604
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-11 09:54:01 -0400

    Abort build when there's an error

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 set /p key= < keys/build_key.txt
 
 py -3.6-32 -m PyInstaller wwrando.spec --key=%key%
+if %errorlevel% neq 0 exit /b %errorlevel%
 py -3.6-32 build.py
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 py -3.6 -m PyInstaller wwrando.spec --key=%key%
+if %errorlevel% neq 0 exit /b %errorlevel%
 py -3.6 build.py
+if %errorlevel% neq 0 exit /b %errorlevel%
