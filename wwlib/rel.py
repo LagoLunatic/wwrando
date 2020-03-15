2fbf047fba69864c5158e4b430b34f7914c1aa80
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-03-15 11:44:51 -0400

    REL: Implement functions to read/write data based on offset from the start of the REL

--- wwlib/rel.py
+++ wwlib/rel.py
@@ -121,6 +121,62 @@ class REL:
     # BSS doesn't start until the next 0x20 byte alignment after the end of the initialized data (specified by fix_size).
     return (self.fix_size + 0x1F) & ~(0x1F)
   
+  def convert_rel_offset_to_section_data_and_relative_offset(self, offset):
+    data = None
+    relative_offset = None
+    
+    for section in self.sections:
+      if section.is_uninitialized:
+        continue
+      
+      if section.offset <= offset < section.offset+section.length:
+        data = section.data
+        relative_offset = offset - section.offset
+        break
+    
+    return (data, relative_offset)
+  
+  def read_data(self, read_callback, offset, *args):
+    # This function allows reading from a REL using the offset from the start of the REL file, instead of needing to know the section index and offset within that section.
+    
+    data, relative_offset = self.convert_rel_offset_to_section_data_and_relative_offset(offset)
+    
+    if data is None:
+      raise Exception("Offset %04X is not in the data for any of the REL sections" % offset)
+    
+    return read_callback(data, relative_offset, *args)
+  
+  def write_data(self, write_callback, offset, *args):
+    # This function allows writing to a REL using the offset from the start of the REL file, instead of needing to know the section index and offset within that section.
+    
+    data, relative_offset = self.convert_rel_offset_to_section_data_and_relative_offset(offset)
+    
+    if data is None and offset > self.relocation_table_offset:
+      # Legacy support for changing a relocation's address.
+      # This should ideally be removed if all the REL asm patches that use it are rewritten.
+      for module_num, relocation_data_entries in self.relocation_entries_for_module.items():
+        for relocation in relocation_data_entries:
+          if offset == relocation.offset+4:
+            if write_callback == write_u32 and isinstance(args[0], int):
+              new_symbol_address = args[0]
+            elif write_callback == write_and_pack_bytes and len(args[0]) == 4:
+              new_bytes = args[0]
+              new_symbol_address = 0
+              new_symbol_address |= new_bytes[0] << 24
+              new_symbol_address |= new_bytes[1] << 16
+              new_symbol_address |= new_bytes[2] << 8
+              new_symbol_address |= new_bytes[3] << 0
+            else:
+              raise Exception("Invalid legacy overwrite of REL relocation: %s, %04X, %s" % (write_callback, offset, args))
+            
+            relocation.symbol_address = new_symbol_address
+            return
+    
+    if data is None:
+      raise Exception("Offset %04X is not in the data for any of the REL sections" % offset)
+    
+    write_callback(data, relative_offset, *args)
+  
   def save_to_file(self, file_path, preserve_section_data_offsets=False):
     self.save_changes(preserve_section_data_offsets=preserve_section_data_offsets)
     
@@ -304,6 +360,8 @@ class RELRelocation:
     self.curr_section_num = None
   
   def read(self, rel_data, offset, prev_relocation_offset, curr_section_num):
+    self.offset = offset
+    
     self.offset_of_curr_relocation_from_prev = read_u16(rel_data, offset+0x00)
     self.relocation_type = RELRelocationType(read_u8(rel_data, offset+0x02))
     self.section_num_to_relocate_against = read_u8(rel_data, offset+0x03)
@@ -313,6 +371,8 @@ class RELRelocation:
     self.curr_section_num = curr_section_num
   
   def save(self, rel_data, offset):
+    self.offset = offset
+    
     if self.relocation_type == RELRelocationType.R_DOLPHIN_SECTION:
       self.offset_of_curr_relocation_from_prev = 0
       self.symbol_address = 0
