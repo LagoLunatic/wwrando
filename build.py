4beec7773ba6bac074c0f5ccbccccbdc0b941ed9
Ethan Armbrust <ethanarm@buffalo.edu>
2021-04-06 16:07:26 -0400

    Support other platforms in build.py

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 
 from zipfile import ZipFile
 import os
+import platform
 
 from randomizer import VERSION_WITHOUT_COMMIT
 
@@ -16,12 +17,15 @@ else:
   base_zip_name = base_name_with_version
 
 zip_name = base_zip_name.replace(" ", "_") + ".zip"
+exe_ext = ""
+if platform.system() == "Windows":
+  exe_ext = ".exe"
 
-exe_path = "./dist/%s.exe" % base_name_with_version
+exe_path = "./dist/%s" % base_name_with_version + exe_ext
 if not os.path.isfile(exe_path):
   raise Exception("Executable not found: %s" % exe_path)
 
 with ZipFile("./dist/" + zip_name, "w") as zip:
-  zip.write(exe_path, arcname="%s.exe" % base_name)
+  zip.write(exe_path, arcname=base_name + exe_ext)
   zip.write("README.md", arcname="README.txt")
   zip.write("./models/About Custom Models.txt", arcname="./models/About Custom Models.txt")
