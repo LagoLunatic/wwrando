2e5e7c5bce0ad6394cfd9a661d1319bb9b5ec1bf
Ethan Armbrust <ethanarm@buffalo.edu>
2021-04-20 18:12:09 -0400

    Use userdata path for macOS only, remove unneeded code

--- wwrando_paths.py
+++ wwrando_paths.py
@@ -1,6 +1,6 @@
 
 import os
-import sys
+import platform
 
 import appdirs
 
@@ -8,8 +8,15 @@ try:
   from sys import _MEIPASS
   RANDO_ROOT_PATH = _MEIPASS
   IS_RUNNING_FROM_SOURCE = False
-  SETTINGS_PATH = os.path.join(".", "settings.txt")
-  CUSTOM_MODELS_PATH = os.path.join(".", "models")
+  if platform.system() == "Darwin":
+    userdata_path = appdirs.user_data_dir("wwrando", "wwrando")
+    SETTINGS_PATH = os.path.join(userdata_path, "settings.txt")
+    CUSTOM_MODELS_PATH = os.path.join(userdata_path, "models")
+    if not os.path.isdir(CUSTOM_MODELS_PATH):
+      os.mkdir(CUSTOM_MODELS_PATH)
+  else:
+    CUSTOM_MODELS_PATH = os.path.join(".", "models")
+    SETTINGS_PATH = os.path.join(".", "settings.txt")
 except ImportError:
   RANDO_ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
   IS_RUNNING_FROM_SOURCE = True
@@ -21,8 +28,3 @@ DATA_PATH = os.path.join(RANDO_ROOT_PATH, "data")
 LOGIC_PATH = os.path.join(RANDO_ROOT_PATH, "logic")
 ASM_PATH = os.path.join(RANDO_ROOT_PATH, "asm")
 SEEDGEN_PATH = os.path.join(RANDO_ROOT_PATH, "seedgen")
-USERDATA_PATH = appdirs.user_data_dir("wwrando", "wwrando")
-if os.path.isdir("./models/"):
-  MODEL_PATH = "./models/"
-else:
-  MODEL_PATH = os.path.join(USERDATA_PATH, "models")
