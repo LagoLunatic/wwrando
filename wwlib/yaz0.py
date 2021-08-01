d4fd8b401891f7251aa6a2fce8fbd323b5b1b0e9
LagoLunatic <LagoLunatic@users.noreply.github.com>
2021-08-01 00:57:03 -0400

    Numerous improvements to Yaz0 compression
    * Added an argument to change the search depth, and change the default from 0x400 to 0x1000 to match the original compressed files. This reduces compressed file sizes slightly, but also makes compression even slower.
    * Added an argument to pad the compressed data to the nearest 0x20 bytes. This isn't needed for Wind Waker, but some other games do need it.
    * Made compression more faithful to the original files. Decompressing and recompressing a file should now result in the exact same bytes as the original file (assuming you use the correct search depth and padding arguments).
    * Cleaned up some code, improved variable names, and added comments.

--- wwlib/yaz0.py
+++ wwlib/yaz0.py
@@ -7,9 +7,14 @@ from fs_helpers import *
 class Yaz0:
   MAX_RUN_LENGTH = 0xFF + 0x12
   
-  num_bytes_1 = 0
-  match_pos = 0
-  prev_flag = False
+  # How far back to search when compressing.
+  # Can search as far back as 0x1000 bytes, but the farther back we search the slower it is.
+  DEFAULT_SEARCH_DEPTH = 0x1000
+  
+  # Variables to hold the reserved next match across loops.
+  next_num_bytes = 0
+  next_match_pos = 0
+  next_flag = False
   
   @staticmethod
   def check_is_compressed(comp_data):
@@ -71,7 +76,7 @@ class Yaz0:
     return BytesIO(uncomp_data)
   
   @staticmethod
-  def compress(uncomp_data):
+  def compress(uncomp_data, search_depth=DEFAULT_SEARCH_DEPTH, should_pad_data=False):
     comp_data = BytesIO()
     write_magic_str(comp_data, 0, "Yaz0", 4)
     
@@ -81,9 +86,9 @@ class Yaz0:
     write_u32(comp_data, 8, 0)
     write_u32(comp_data, 0xC, 0)
     
-    Yaz0.num_bytes_1 = 0
-    Yaz0.match_pos = 0
-    Yaz0.prev_flag = False
+    Yaz0.next_num_bytes = 0
+    Yaz0.next_match_pos = None
+    Yaz0.next_flag = False
     
     uncomp_offset = 0
     uncomp = read_and_unpack_bytes(uncomp_data, 0, uncomp_size, "B"*uncomp_size)
@@ -92,7 +97,7 @@ class Yaz0:
     valid_bit_count = 0
     curr_code_byte = 0
     while uncomp_offset < uncomp_size:
-      num_bytes, match_pos = Yaz0.get_num_bytes_and_match_pos(uncomp, uncomp_offset)
+      num_bytes, match_pos = Yaz0.get_num_bytes_and_match_pos(uncomp, uncomp_offset, search_depth=search_depth)
       
       if num_bytes < 3:
         # Copy the byte directly
@@ -111,7 +116,7 @@ class Yaz0:
             num_bytes = Yaz0.MAX_RUN_LENGTH
           dst.append(num_bytes - 0x12)
         else:
-          byte = (((num_bytes - 2) << 4) | (dist >> 8) & 0xFF)
+          byte = (((num_bytes - 2) << 4) | (dist >> 8) & 0x0F)
           dst.append(byte)
           dst.append(dist & 0xFF)
         
@@ -140,55 +145,60 @@ class Yaz0:
       for byte in dst:
         write_u8(comp_data, comp_offset, byte)
         comp_offset += 1
+    else:
+      # If there are no codes leftover to be written, we instead write a single zero at the end for some reason.
+      # I don't think it's necessary in practice, but we do it for maximum accuracy with the original algorithm.
+      write_u8(comp_data, comp_offset, 0)
+      comp_offset += 1
+    
+    if should_pad_data:
+      align_data_to_nearest(comp_data, 0x20, padding_bytes=b'\0')
     
     return comp_data
   
   @staticmethod
-  def get_num_bytes_and_match_pos(uncomp, uncomp_offset):
+  def get_num_bytes_and_match_pos(uncomp, uncomp_offset, search_depth=DEFAULT_SEARCH_DEPTH):
     num_bytes = 1
     
-    if Yaz0.prev_flag:
-      Yaz0.prev_flag = False
-      return (Yaz0.num_bytes_1, Yaz0.match_pos)
+    if Yaz0.next_flag:
+      Yaz0.next_flag = False
+      return (Yaz0.next_num_bytes, Yaz0.next_match_pos)
     
-    Yaz0.prev_flag = False
-    num_bytes, Yaz0.match_pos = Yaz0.simple_rle_encode(uncomp, uncomp_offset)
-    match_pos = Yaz0.match_pos
+    Yaz0.next_flag = False
+    num_bytes, match_pos = Yaz0.simple_rle_encode(uncomp, uncomp_offset, search_depth=search_depth)
     
     if num_bytes >= 3:
-      Yaz0.num_bytes_1, Yaz0.match_pos = Yaz0.simple_rle_encode(uncomp, uncomp_offset+1)
+      # Check if the next byte has a match that would compress better than the current byte.
+      Yaz0.next_num_bytes, Yaz0.next_match_pos = Yaz0.simple_rle_encode(uncomp, uncomp_offset+1, search_depth=search_depth)
       
-      if Yaz0.num_bytes_1 >= num_bytes+2:
+      if Yaz0.next_num_bytes >= num_bytes+2:
+        # If it does, then only copy one byte for this match and reserve the next match for later so we save more space.
         num_bytes = 1
-        Yaz0.prev_flag = True
+        match_pos = None
+        Yaz0.next_flag = True
     
     return (num_bytes, match_pos)
   
   @staticmethod
-  def simple_rle_encode(uncomp, uncomp_offset):
-    # How far back to search. Can search as far back as 0x1000 bytes, but the farther back we search the slower it is.
-    start_offset = uncomp_offset - 0x400
+  def simple_rle_encode(uncomp, uncomp_offset, search_depth=DEFAULT_SEARCH_DEPTH):
+    start_offset = uncomp_offset - search_depth
     if start_offset < 0:
       start_offset = 0
     
-    num_bytes = 1
-    match_pos = 0
+    num_bytes = 0
+    match_pos = None
+    max_num_bytes_to_check = len(uncomp) - uncomp_offset
+    if max_num_bytes_to_check > Yaz0.MAX_RUN_LENGTH:
+      max_num_bytes_to_check = Yaz0.MAX_RUN_LENGTH
     
-    for i in range(start_offset, uncomp_offset):
-      for j in range(len(uncomp) - uncomp_offset):
-        if uncomp[i + j] != uncomp[uncomp_offset + j]:
+    for possible_match_pos in range(start_offset, uncomp_offset):
+      for index_in_match in range(max_num_bytes_to_check):
+        if uncomp[possible_match_pos + index_in_match] != uncomp[uncomp_offset + index_in_match]:
           break
-        #if j == Yaz0.MAX_RUN_LENGTH:
-        #  break
-      
-      if j > num_bytes:
-        num_bytes = j
-        match_pos = i
         
-        #if num_bytes == Yaz0.MAX_RUN_LENGTH:
-        #  break
-    
-    #if num_bytes == 2:
-    #  num_bytes = 1
+        num_bytes_matched = index_in_match + 1
+        if num_bytes_matched > num_bytes:
+          num_bytes = num_bytes_matched
+          match_pos = possible_match_pos
     
     return (num_bytes, match_pos)
