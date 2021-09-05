1d8d20983d91e5784e8fa1a37b6750adab40aab8
LagoLunatic <LagoLunatic@users.noreply.github.com>
2021-09-05 15:41:11 -0400

    Fix corrupting model textures under certain circumstances
    If a model had duplicate texture headers under the same name, and the image or palette format of one of the duplicates was changed but the others sharing the same name were not, the headers were incorrectly being given the same image/palette data offsets, corrupting one of them.

--- wwlib/j3d.py
+++ wwlib/j3d.py
@@ -284,14 +284,15 @@ class TEX1(J3DChunk):
     image_data_offsets = {}
     for i, texture in enumerate(self.textures):
       filename = self.texture_names[i]
-      if filename in image_data_offsets:
-        texture.image_data_offset = image_data_offsets[filename] - texture.header_offset
+      format_and_filename = "%X_%s" % (texture.image_format.value, filename)
+      if format_and_filename in image_data_offsets:
+        texture.image_data_offset = image_data_offsets[format_and_filename] - texture.header_offset
         continue
       
       self.data.seek(next_available_data_offset)
       
       texture.image_data_offset = next_available_data_offset - texture.header_offset
-      image_data_offsets[filename] = next_available_data_offset
+      image_data_offsets[format_and_filename] = next_available_data_offset
       texture.image_data.seek(0)
       self.data.write(texture.image_data.read())
       align_data_to_nearest(self.data, 0x20)
@@ -300,15 +301,16 @@ class TEX1(J3DChunk):
     palette_data_offsets = {}
     for i, texture in enumerate(self.textures):
       filename = self.texture_names[i]
-      if filename in palette_data_offsets:
-        texture.palette_data_offset = palette_data_offsets[filename] - texture.header_offset
+      format_and_filename = "%X_%s" % (texture.palette_format.value, filename)
+      if format_and_filename in palette_data_offsets:
+        texture.palette_data_offset = palette_data_offsets[format_and_filename] - texture.header_offset
         continue
       
       self.data.seek(next_available_data_offset)
       
       if texture.needs_palettes():
         texture.palette_data_offset = next_available_data_offset - texture.header_offset
-        palette_data_offsets[filename] = next_available_data_offset
+        palette_data_offsets[format_and_filename] = next_available_data_offset
         texture.palette_data.seek(0)
         self.data.write(texture.palette_data.read())
         align_data_to_nearest(self.data, 0x20)
@@ -317,7 +319,7 @@ class TEX1(J3DChunk):
         # If the image doesn't use palettes its palette offset is just the same as the first texture's image offset.
         first_texture = self.textures[0]
         texture.palette_data_offset = first_texture.image_data_offset + first_texture.header_offset - texture.header_offset
-        palette_data_offsets[filename] = first_texture.image_data_offset + first_texture.header_offset
+        palette_data_offsets[format_and_filename] = first_texture.image_data_offset + first_texture.header_offset
     
     for texture in self.textures:
       texture.save_header_changes()
