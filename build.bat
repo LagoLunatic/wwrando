fe590027564007c14c0fab597bb7104a0fe6b8e7
LagoLunatic <LagoLunatic@users.noreply.github.com>
2022-01-13 17:20:19 -0500

    Actions: Fix build key being publicly logged
    PyInstaller's default log level (INFO) logs the build key to the console, which is visible to anyone with a GitHub account via the actions tab. I've switched it to the WARN log level, which does not print this information.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-py -3.9 -m PyInstaller wwrando.spec
+py -3.9 -m PyInstaller --log-level=WARN wwrando.spec
 if %errorlevel% neq 0 exit /b %errorlevel%
 py -3.9 build.py
 if %errorlevel% neq 0 exit /b %errorlevel%
