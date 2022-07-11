3f7d7f585d9323bd7e3a664cbc23e8af61e52e03
LagoLunatic <LagoLunatic@users.noreply.github.com>
2022-07-11 17:27:39 -0400

    Fix bash script to generate keys

--- keys/generate_keys.sh
+++ keys/generate_keys.sh
@@ -1,19 +1,8 @@
-python generate_key.py > temp_key
-key_1= < temp_key
-rm temp_key
+key_1=$(python generate_key.py)
+key_2=$(python generate_key.py)
+key_3=$(python generate_key.py)
+key_4=$(python generate_key.py)
 
-python generate_key.py > temp_key
-key_2= < temp_key
-rm temp_key
-
-python generate_key.py > temp_key
-key_3= < temp_key
-rm temp_key
-
-python generate_key.py > temp_key
-key_4= < temp_key
-rm temp_key
-
-echo SEED_KEY=str(0X%key_1%-(0X%key_2%+0X%key_3%)*0X%key_4%) > seed_key.py
+echo "SEED_KEY=str(0X$key_1-(0X$key_2+0X$key_3)*0X$key_4)" > seed_key.py
 
 python generate_key.py 8 > build_key.txt
