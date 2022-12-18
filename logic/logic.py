LagoLunatic/wwrando

commit 18fc8993515669e08a878b12c0acbb0c57a297e5

Never load item locs/macros YAML files more than once

logic/logic.py
@@ -46,6 +46,9 @@ class Logic:
     ]),
   ])
   
+  initial_item_locations = None
+  initial_macros = None
+  
   def __init__(self, rando):
     self.rando = rando
     self.requirement_met_cache = {}
@@ -657,6 +660,9 @@ class Logic:
   
   @staticmethod
   def load_and_parse_item_locations():
+    if Logic.initial_item_locations is not None:
+      return Logic.initial_item_locations.copy()
+    
     with open(os.path.join(LOGIC_PATH, "item_locations.txt")) as f:
       item_locations = yaml.load(f, YamlOrderedDictLoader)
     
@@ -671,14 +677,23 @@ class Logic:
       types = [type.strip() for type in types]
       item_locations[location_name]["Types"] = types
     
+    Logic.initial_item_locations = item_locations.copy()
     return item_locations
     
   def load_and_parse_macros(self):
+    if Logic.initial_macros is not None:
+      self.macros = Logic.initial_macros.copy()
+      return self.macros
+    
     with open(os.path.join(LOGIC_PATH, "macros.txt")) as f:
       macro_strings = yaml.safe_load(f)
+    
     self.macros = {}
     for macro_name, req_string in macro_strings.items():
       self.set_macro(macro_name, req_string)
+    
+    Logic.initial_macros = self.macros.copy()
+    return self.macros
   
   def set_macro(self, macro_name, req_string):
     self.macros[macro_name] = Logic.parse_logic_expression(req_string)

commit 66d2b9d2f4b7a0d5954e7d6075537be1894443cd

Use deepcopy instead of copy
This fixes the potential for a major bug I introduced in 18fc8993515669e08a878b12c0acbb0c57a297e5 since item locations and macros are nested objects and are not properly copied by `copy`. If a macro was changed in one logic but not another, this could result in logic bugs.

I also use deepcopy for saving/loading the simulated playthrough state. This isn't currently necessary, but could theoretically be in the future.

deepcopy only adds about 0.1 seconds of hint generation times compared to copy.

logic/logic.py
@@ -183,12 +183,12 @@ class Logic:
       "unplaced_fixed_consumable_items",
       "requirement_met_cache",
     ]:
-      vars_backup[attr_name] = getattr(self, attr_name).copy()
+      vars_backup[attr_name] = copy.deepcopy(getattr(self, attr_name))
     return vars_backup
   
   def load_simulated_playthrough_state(self, vars_backup):
     for attr_name, value in vars_backup.items():
-      setattr(self, attr_name, value.copy())
+      setattr(self, attr_name, copy.deepcopy(value))
   
   def is_dungeon_or_cave(self, location_name):
     # Look up the setting that the location name is under
@@ -677,7 +677,7 @@ class Logic:
   @staticmethod
   def load_and_parse_item_locations():
     if Logic.initial_item_locations is not None:
-      return Logic.initial_item_locations.copy()
+      return copy.deepcopy(Logic.initial_item_locations)
     
     with open(os.path.join(LOGIC_PATH, "item_locations.txt")) as f:
       item_locations = yaml.load(f, YamlOrderedDictLoader)
@@ -693,12 +693,12 @@ class Logic:
       types = [type.strip() for type in types]
       item_locations[location_name]["Types"] = types
     
-    Logic.initial_item_locations = item_locations.copy()
+    Logic.initial_item_locations = copy.deepcopy(item_locations)
     return item_locations
     
   def load_and_parse_macros(self):
     if Logic.initial_macros is not None:
-      self.macros = Logic.initial_macros.copy()
+      self.macros = copy.deepcopy(Logic.initial_macros)
       return self.macros
     
     with open(os.path.join(LOGIC_PATH, "macros.txt")) as f:
@@ -708,7 +708,7 @@ class Logic:
     for macro_name, req_string in macro_strings.items():
       self.set_macro(macro_name, req_string)
     
-    Logic.initial_macros = self.macros.copy()
+    Logic.initial_macros = copy.deepcopy(self.macros)
     return self.macros
   
   def set_macro(self, macro_name, req_string):