7969d65e85038cfa22731f960aa246c028c05700
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-07-25 02:17:59 -0400

    Make module init all static
    Apparently making this dynamic breaks PyInstaller builds.

--- wwlib/__init__.py
+++ wwlib/__init__.py
@@ -2,13 +2,20 @@
 import os
 import glob
 
-__all__ = []
+__all__ = [
+  "charts",
+  "dzb",
+  "dzx",
+  "events",
+  "stage_searcher",
+]
+
 for module_path in glob.glob(glob.escape(os.path.dirname(__file__)) + "/*.py"):
   module_name, file_ext = os.path.splitext(os.path.basename(module_path))
   assert file_ext == ".py"
   if module_name == "__init__":
     continue
-  __all__.append(module_name)
+  assert module_name in __all__, f"{module_name} missing from __init__"
 
 # We need to import all the file types so that they register themselves with the RARC class.
 from . import *
