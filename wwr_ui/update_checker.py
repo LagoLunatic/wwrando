9c0210ec311ac5f56f415b3bdb1f1a49dae22ded
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-08-10 03:44:49 -0400

    Remove dependency on deprecated distutils

--- wwr_ui/update_checker.py
+++ wwr_ui/update_checker.py
@@ -4,31 +4,38 @@ import ssl
 import certifi
 import json
 import traceback
-from distutils.version import LooseVersion
 
 from version import VERSION
 
 LATEST_RELEASE_DOWNLOAD_PAGE_URL = "https://github.com/LagoLunatic/wwrando/releases/latest"
 LATEST_RELEASE_API_URL = "https://api.github.com/repos/lagolunatic/wwrando/releases/latest"
 
+def string_to_version(string: str):
+  string = string.removeprefix('v')
+  if "-BETA" in string:
+    string = string.split("-BETA")[0]
+  if "_" in string:
+    string = string.split("_")[0]
+  version = tuple(int(e) for e in string.split('.'))
+  return version
+
 def check_for_updates():
   try:
     with urllib.request.urlopen(LATEST_RELEASE_API_URL, context=ssl.create_default_context(cafile=certifi.where())) as page:
       data = json.loads(page.read().decode())
       
-      latest_version_name = data["tag_name"]
-      if latest_version_name[0] == "v":
-        latest_version_name = latest_version_name[1:]
+      curr_version = string_to_version(VERSION)
+      latest_version = string_to_version(data["tag_name"])
       
       if "-BETA" in VERSION:
-        version_without_beta = VERSION.split("-BETA")[0]
-        if LooseVersion(latest_version_name) >= LooseVersion(version_without_beta):
-          return latest_version_name
+        print(latest_version >= curr_version)
+        if latest_version >= curr_version:
+          return '.'.join(str(e) for e in latest_version)
         else:
           return None
       else:
-        if LooseVersion(latest_version_name) > LooseVersion(VERSION):
-          return latest_version_name
+        if latest_version > curr_version:
+          return '.'.join(str(e) for e in latest_version)
         else:
           return None
   except Exception as e:
