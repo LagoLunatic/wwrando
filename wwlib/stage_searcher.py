bc7d626f842b9aa40f290a69ffa9625eb81fb7c0
LagoLunatic <LagoLunatic@users.noreply.github.com>
2023-09-04 19:25:03 -0400

    Fix how weird pots that drop no item are printed out

--- wwlib/stage_searcher.py
+++ wwlib/stage_searcher.py
@@ -387,6 +387,8 @@ def print_all_used_item_pickup_flags(self):
               elif class_name in ["d_a_tsubo", "d_a_switem", "d_a_obj_barrel2", "d_a_obj_movebox", "d_a_obj_homen", "d_a_stone", "d_a_stone2", "d_a_obj_bemos"]:
                 if actor.dropped_item_id < 0x20:
                   item_name = self.item_names[actor.dropped_item_id]
+                elif actor.dropped_item_id == 0x3F:
+                  item_name = "(No item)"
                 else:
                   item_name = "Random drop type 0x%02X" % (actor.dropped_item_id - 0x20)
               elif class_name == "d_a_deku_item":
